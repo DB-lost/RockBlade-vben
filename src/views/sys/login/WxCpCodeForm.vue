c6f478d1d56ce51e97ae1faaf7879ddfa1b9067a
DB <2502523450@qq.com>
2024-07-03 19:38:38 +0800

    fix: 调整企业微信相关代码

--- src/views/sys/login/WxCpCodeForm.vue
+++ src/views/sys/login/WxCpCodeForm.vue
@@ -1,38 +1,73 @@
 <template>
-  <div v-if="getShow">
+  <div v-show="getShow">
     <LoginFormTitle class="enter-x" />
-    <div class="enter-x min-w-64 min-h-64">
-      <QrCode
-        :value="qrCodeUrl"
-        class="enter-x flex justify-center xl:justify-start"
-        :width="280"
-      />
-      <Divider class="enter-x">{{ t('sys.login.scanSign') }}</Divider>
-      <Button size="large" block class="mt-4 enter-x" @click="handleBackLogin">
-        {{ t('sys.login.backSignIn') }}
-      </Button>
-    </div>
+    <div id="ww_login"></div>
   </div>
 </template>
 <script lang="ts" setup>
-  import { computed, nextTick, unref } from 'vue';
+  import { computed, unref, onMounted, ref } from 'vue';
   import LoginFormTitle from './LoginFormTitle.vue';
-  import { Button, Divider } from 'ant-design-vue';
-  import { QrCode } from '@/components/Qrcode';
-  import { useI18n } from '@/hooks/web/useI18n';
   import { useLoginState, LoginStateEnum } from './useLogin';
-  import { getWxCpQrcode } from '@/api/sys/user';
-
-  let qrCodeUrl = '';
-
-  nextTick(() => {
-    getWxCpQrcode().then((res) => {
-      qrCodeUrl = res;
-    });
-  });
+  //企业微信相关组件
+  import * as ww from '@wecom/jssdk';
+  import { WWLoginRedirectType, WWLoginType } from '@wecom/jssdk';
+  import { useI18n } from 'vue-i18n';
+  import { useUserStore } from '@/store/modules/user';
+  import { useMessage } from '@/hooks/web/useMessage';
+  import { useDesign } from '@/hooks/web/useDesign';
 
   const { t } = useI18n();
-  const { handleBackLogin, getLoginState } = useLoginState();
+  const { getLoginState } = useLoginState();
 
   const getShow = computed(() => unref(getLoginState) === LoginStateEnum.WX_CP_LOGIN);
+  const loading = ref(false);
+  const userStore = useUserStore();
+  const { notification, createErrorModal } = useMessage();
+  const { prefixCls } = useDesign('login');
+  onMounted(async () => {
+    ww.createWWLoginPanel({
+      el: '#ww_login',
+      params: {
+        login_type: WWLoginType.corpApp,
+        appid: 'ww35e414c950ec774e',
+        agentid: '1000004',
+        redirect_uri: 'https://system.rockblade.cn',
+        state: 'loginState',
+        redirect_type: WWLoginRedirectType.callback,
+        scope: 'snsapi_privateinfo',
+      },
+      onCheckWeComLogin({ isWeComLogin }) {
+        console.log(isWeComLogin);
+      },
+      async onLoginSuccess({ code }) {
+        try {
+          loading.value = true;
+          const userInfo = await userStore.wxCpLogin({
+            code: code,
+            username: undefined,
+            password: undefined,
+            mode: 'none', //不要默认的错误提示
+          });
+          if (userInfo) {
+            notification.success({
+              message: t('sys.login.loginSuccessTitle'),
+              description: `${t('sys.login.loginSuccessDesc')}: ${userInfo.realName}`,
+              duration: 3,
+            });
+          }
+        } catch (error) {
+          createErrorModal({
+            title: t('sys.api.errorTip'),
+            content: (error as unknown as Error).message || t('sys.api.networkExceptionMsg'),
+            getContainer: () => document.body.querySelector(`.${prefixCls}`) || document.body,
+          });
+        } finally {
+          loading.value = false;
+        }
+      },
+      onLoginFail(err) {
+        console.log(err);
+      },
+    });
+  });
 </script>
